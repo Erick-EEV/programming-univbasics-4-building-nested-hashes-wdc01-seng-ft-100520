9f559fe683c2d37ac5a18e3b5579d028c312201c
Erick Villeda <eeverickeev@gmail.com>
2020-09-03 12:51:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,11 @@ def base_hash
 end
 def monopoly_with_second_tier
   base_hash = {
-    :railroads => {}
+    :railroads => {
+      :pieces =>{4}
+      :rent_in_dollars => {}
+      :names => {}
+    }
   }
 end
 
